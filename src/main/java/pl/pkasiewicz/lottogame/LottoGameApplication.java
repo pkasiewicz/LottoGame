c85415949cd8e147484d4354966dc3c6c1ad42a8
piotrkasiewicz <ptrkasiewicz@gmail.com>
2025-09-26 23:49:12 +0200

    Initialize LottoGame project with Maven, Spring Boot, and basic configurations

---
+++ src/main/java/pl/pkasiewicz/lottogame/LottoGameApplication.java
@@ -0,0 +1,13 @@
+package pl.pkasiewicz.lottogame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottoGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LottoGameApplication.class, args);
+    }
+
+}
