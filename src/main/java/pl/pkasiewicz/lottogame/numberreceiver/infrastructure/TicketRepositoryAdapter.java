0dfebbc33fe34f64ff2013e92cbfb256441a8d00
piotrkasiewicz <ptrkasiewicz@gmail.com>
2025-10-30 19:41:01 +0100

    feat(number-receiver): implement TicketEntity, TicketJpaRepository, and TicketRepositoryAdapter for lottery ticket management

---
+++ src/main/java/pl/pkasiewicz/lottogame/numberreceiver/infrastructure/TicketRepositoryAdapter.java
@@ -0,0 +1,38 @@
+package pl.pkasiewicz.lottogame.numberreceiver.infrastructure;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import pl.pkasiewicz.lottogame.numberreceiver.domain.Ticket;
+import pl.pkasiewicz.lottogame.numberreceiver.domain.TicketRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class TicketRepositoryAdapter implements TicketRepository {
+
+    private final TicketJpaRepository repository;
+
+
+    @Override
+    public Ticket save(Ticket ticket) {
+        TicketEntity saved = repository.save(TicketEntity.fromDomain(ticket));
+        return saved.toDomain();
+    }
+
+    @Override
+    public List<Ticket> findAllTicketsByDrawDate(LocalDateTime drawDate) {
+        return repository.findAllTicketsByDrawDate(drawDate).stream()
+                .map(TicketEntity::toDomain)
+                .toList();
+    }
+
+    @Override
+    public Optional<Ticket> findById(UUID id) {
+        return repository.findById(id)
+                .map(TicketEntity::toDomain);
+    }
+}
