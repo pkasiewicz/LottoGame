47760c9e2fa0f50e34030fb12e822a1c2f0e4441
piotrkasiewicz <ptrkasiewicz@gmail.com>
2025-11-05 22:10:36 +0100

    feat(result-checker): add TicketResultEntity class for persistence

---
+++ src/main/java/pl/pkasiewicz/lottogame/resultchecker/infrastructure/TicketResultEntity.java
@@ -0,0 +1,75 @@
+package pl.pkasiewicz.lottogame.resultchecker.infrastructure;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import pl.pkasiewicz.lottogame.resultchecker.domain.TicketResult;
+import pl.pkasiewicz.lottogame.resultchecker.domain.TicketResultId;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+class TicketResultEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private UUID ticketId;
+
+    @ElementCollection
+    @CollectionTable(
+            name = "ticket_result_user_numbers",
+            joinColumns = @JoinColumn(name = "ticket_result_id")
+    )
+    @Column(name = "user_numbers", nullable = false)
+    private Set<Integer> userNumbers;
+
+    @ElementCollection
+    @CollectionTable(
+            name = "user_hit_numbers",
+            joinColumns = @JoinColumn(name = "ticket_result_id")
+    )
+    @Column(name = "hit_numbers", nullable = false)
+    private Set<Integer> hitNumbers;
+
+    @Column(nullable = false)
+    private int hitCount;
+
+    @Column(nullable = false)
+    private LocalDateTime drawDate;
+
+    @Column(nullable = false)
+    private boolean isWinner;
+
+    public TicketResult toDomain() {
+        return new TicketResult(
+                new TicketResultId(id),
+                ticketId,
+                userNumbers,
+                hitNumbers,
+                hitCount,
+                drawDate,
+                isWinner
+        );
+    }
+
+    public static TicketResultEntity fromDomain(TicketResult ticketResult) {
+        return new TicketResultEntity(
+                ticketResult.getId().value(),
+                ticketResult.getTicketId(),
+                ticketResult.getUserNumbers(),
+                ticketResult.getHitNumbers(),
+                ticketResult.getHitCount(),
+                ticketResult.getDrawDate(),
+                ticketResult.isWinner()
+        );
+    }
+}
