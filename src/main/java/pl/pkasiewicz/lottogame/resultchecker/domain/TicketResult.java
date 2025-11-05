71a6d08066455f6beed597ec790588dcd5c86c52
piotrkasiewicz <ptrkasiewicz@gmail.com>
2025-11-05 22:09:28 +0100

    feat(result-checker): add TicketResult and TicketResultId classes

---
+++ src/main/java/pl/pkasiewicz/lottogame/resultchecker/domain/TicketResult.java
@@ -0,0 +1,37 @@
+package pl.pkasiewicz.lottogame.resultchecker.domain;
+
+import lombok.Data;
+import pl.pkasiewicz.lottogame.numberreceiver.domain.Ticket;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Data
+public class TicketResult {
+
+    private final TicketResultId id;
+    private final UUID ticketId;
+    private final Set<Integer> userNumbers;
+    private final Set<Integer> hitNumbers;
+    private final int hitCount;
+    private final LocalDateTime drawDate;
+    private final boolean isWinner;
+
+    public static TicketResult fromTicket(Ticket ticket, Set<Integer> winningNumbers) {
+        Set<Integer> hitNumbers = ticket.getNumbers().stream()
+                .filter(winningNumbers::contains)
+                .collect(Collectors.toSet());
+
+        return new TicketResult(
+                new TicketResultId(UUID.randomUUID()),
+                ticket.getId().value(),
+                ticket.getNumbers(),
+                hitNumbers,
+                hitNumbers.size(),
+                ticket.getDrawDate(),
+                hitNumbers.size() >= 3
+        );
+    }
+}
