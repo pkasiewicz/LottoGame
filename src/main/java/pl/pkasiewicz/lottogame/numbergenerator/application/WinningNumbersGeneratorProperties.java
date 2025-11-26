f709e6bc407723e867b40507d53294f0e3bdb937
piotrkasiewicz <ptrkasiewicz@gmail.com>
2025-11-26 22:39:08 +0100

    refactor: rename NumberGenerator classes and properties to WinningNumbers for clarity

--- src/main/java/pl/pkasiewicz/lottogame/numbergenerator/application/WinningNumbersGeneratorProperties.java
+++ src/main/java/pl/pkasiewicz/lottogame/numbergenerator/application/WinningNumbersGeneratorProperties.java
@@ -3,7 +3,7 @@ package pl.pkasiewicz.lottogame.numbergenerator.application;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "lotto.numbers-generator")
-public record NumberGeneratorProperties(
+public record WinningNumbersGeneratorProperties(
         int count,
         int lowerBand,
         int upperBand) {
