2b1e7bf13514395b8dab327bc1ce93f614b0eec6
piotrkasiewicz <ptrkasiewicz@gmail.com>
2025-11-26 22:49:49 +0100

    refactor: remove unused import for DrawDateGenerator in ResultCheckerFacadeTest

--- src/test/java/pl/pkasiewicz/lottogame/resultchecker/application/ResultCheckerFacadeTest.java
+++ src/test/java/pl/pkasiewicz/lottogame/resultchecker/application/ResultCheckerFacadeTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pl.pkasiewicz.lottogame.domain.DrawDateGenerable;
-import pl.pkasiewicz.lottogame.infrastructure.DrawDateGenerator;
 import pl.pkasiewicz.lottogame.numbergenerator.domain.WinningNumbers;
 import pl.pkasiewicz.lottogame.numbergenerator.domain.WinningNumbersGeneratorUseCase;
 import pl.pkasiewicz.lottogame.numbergenerator.domain.WinningNumbersId;
