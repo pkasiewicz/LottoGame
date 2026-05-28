d8400bc3771c257bf635e511f32a1fe07fbd9119
piotrkasiewicz <ptrkasiewicz@gmail.com>
2026-05-28 21:42:24 +0200

    fix: add RestTemplate timeouts in NumbersGeneratorConfiguration

--- src/main/java/pl/pkasiewicz/lottogame/numbergenerator/application/NumbersGeneratorConfiguration.java
+++ src/main/java/pl/pkasiewicz/lottogame/numbergenerator/application/NumbersGeneratorConfiguration.java
@@ -2,6 +2,7 @@ package pl.pkasiewicz.lottogame.numbergenerator.application;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,6 +10,9 @@ public class NumbersGeneratorConfiguration {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(3000);
+        factory.setReadTimeout(3000);
+        return new RestTemplate(factory);
     }
 }
